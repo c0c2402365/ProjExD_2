31e7ab8947feda0db1e01959873881be0cdfb8fa
ibuki kudo <coc24023@edu.teu.ac.jp>
2025-11-25 14:17:33 +0900

    練習2爆弾作成と移動

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,6 +1,8 @@
 import os
+import random
 import sys
 import pygame as pg
+
 
 
 WIDTH, HEIGHT = 1100, 650
@@ -20,6 +22,13 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+    bb_img = pg.Surface((20, 20))  # 空のサーフェス
+    pg.draw.circle(bb_img,(255,0,0),(10,10),10)  # 半径10の赤い円
+    bb_img.set_colorkey((0, 0, 0))
+    bb_rct = bb_img.get_rect() # 爆弾rect
+    bb_rct.centerx = random.randint(0, WIDTH)  # 爆弾のx座標
+    bb_rct.centery = random.randint(0, HEIGHT) # 爆弾のy座標
+    vx, vy = +5, +5  # 爆弾の速度
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -37,6 +46,15 @@ def main():
                 sum_mv[0] += MOVE[0]  # 横方向の移動
                 sum_mv[1] += MOVE[1]  # 縦方向の移動
 
+        kk_rct.move_ip(sum_mv)
+        screen.blit(kk_img, kk_rct)
+        bb_rct.move_ip(vx, vy)
+        screen.blit(bb_img, bb_rct)
+        pg.display.update()
+        tmr += 1
+        clock.tick(50)
+        
+
         # if key_lst[pg.K_UP]:
         #     sum_mv[1] -= 5
         # if key_lst[pg.K_DOWN]:
@@ -47,6 +65,7 @@ def main():
         #     sum_mv[0] += 5
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
+        screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
